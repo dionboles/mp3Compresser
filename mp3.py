37829c370454d1dff25eff2c3d1f06dd3b160384
Dion Boles <dionboles@iMac-2.home>
2019-01-14 13:25:31 -0500

    bug fix

--- mp3.py
+++ mp3.py
@@ -1,24 +1,24 @@
+#!/usr/local/bin/python
+# This Python file uses the following encoding: utf-8
 import os,sys,shutil
+
 if os.path.isdir("mp3Compress") is False:
+    print("Making folder")
     os.mkdir("mp3Compress")
-    os.chdir("mp3Compress")
+    print("Moving to folder")
 else:
-    os.chdir("mp3Compress")
+    print("Folder already created")
+    print("Moving to folder")
+   
+def getText(s):
+    return os.path.splitext(s)[1]
 
-
-getFile = sys.argv[1]
-print(os.path.splitext(getFile)[1][1:])
-print(os.path.realpath(sys.argv[1]))
-if os.path.isfile(getFile) is True:
-    print(getFile)
-    shutil.copy2(getFile,os.path.split(getFile)[1])
-    if os.path.isdir(".") is True:
-        for i in os.listdir('.'):
-            extension = os.path.splitext(i)[1][1:]
-            print(extension)
-            if extension == "mp3":
-                name = str(os.path.splitext(i)[0]+"_compress");
-                print(name)
-                os.system("ffmpeg -i "+i+"  -acodec libmp3lame -ac 2 -ab 64k -ar 44100  "+name+".mp3");
-else:
-    print("Not a file")
+for root, dirs, files in os.walk("."):
+    for name in files:
+        file_extension = str(os.path.splitext(name)[1])
+        if os.path.isfile(name) is True:
+            if file_extension == ".mp3":
+                newName =str(os.path.splitext(name)[0]+"_compress");
+                os.system("ffmpeg -i "+name+"  -acodec libmp3lame -ac 2 -ab 64k -ar 44100  "+newName+".mp3");
+                shutil.copy2("./"+newName+".mp3","./mp3Compress/"+newName+".mp3")
+                os.remove(newName+".mp3");
